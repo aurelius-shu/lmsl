358198f51b9fa9f3bdcadf16904f06f9a8582a7c
aurelius-shu <aureliusshu@gmail.com>
2020-12-15 23:35:30 +0800

    update oop senior __slots__

--- python/liaoxuefeng/10. OOP senior/slots.py
+++ python/liaoxuefeng/10. OOP senior/slots.py
@@ -1,11 +1,25 @@
 class Student(object):
-    __slots__ = ('name')
+    __slots__ = ('name', 'age')
 
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
 
+class StudentA(Student):
+    # __slots__ = ('score', 'a')
+    pass
+
+
+class StudentB(Student):
+    __slots__ = ('score', 'a')
+
+
 if __name__ == "__main__":
-    s = Student('Aurelius', 18)
-    print(s.name, s.age)
+    a = Student('Aurelius', 18)
+    print(a.name, a.age)
+    b = StudentB('Aurelius', 18)
+    b.score = 100
+    print(b.name, b.age, b.score)
+    b.scores = [99, 100]
+    print(b.scores)
